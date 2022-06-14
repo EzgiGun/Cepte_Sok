3b74b5dcead7052adafe538764d7ea6e78bf7a9a
Ezgi Gün <80538645+EzgiGun@users.noreply.github.com>
2022-06-14 23:53:04 +0300

    Add files via upload

---
+++ eShopOnContainers.Core/Controls/AddBasketButton.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace eShopOnContainers.Core.Controls
+{
+    public partial class AddBasketButton : Grid
+    {
+        public AddBasketButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
